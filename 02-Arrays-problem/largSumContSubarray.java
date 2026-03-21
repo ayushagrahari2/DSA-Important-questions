12350b2035fecff883b2cbf1bcc6dff5acc40119
Ayush agrahari <coderayush225@gmail.com>
2026-03-21 12:34:08 -0400

    updated kadane algorithm with range

--- 02-Arrays-problem/largSumContSubarray.java
+++ 02-Arrays-problem/largSumContSubarray.java
@@ -8,8 +8,71 @@ Output:6(Subarray:[4,-1,2,1])
 
 */
 
-public class largSumContSubarray{
-    public static void main(String args[]){
-        
+import java.util.Arrays;
+
+public class largSumContSubarray {
+
+    // kanada's algo [ negative ko reset kr do ]
+
+    public static int largestSum(int[] arr) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            currSum += arr[i];
+            if (currSum > maxSum) {
+                maxSum = currSum;
+            }
+            if (currSum < 0) {
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
+    // agr negative bhi krna ho to
+
+    public static int largestSumNeg(int[] arr) {
+        int currSum = arr[0];
+        int maxSum = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            currSum = Math.max(arr[i], currSum + arr[i]);
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;
+    }
+    // subArrays ka range bhi puche to
+
+    public static int[] largestSumRange(int[] arr){
+
+    int currSum = arr[0];
+    int maxSum = arr[0];
+
+    int start = 0, end = 0;
+    int tempStart = 0;
+
+    for(int i = 1; i < arr.length; i++){
+
+        if(arr[i] > currSum + arr[i]){
+            currSum = arr[i];
+            tempStart = i;
+        } else {
+            currSum = currSum + arr[i];
+        }
+
+        if(currSum > maxSum){
+            maxSum = currSum;
+            start = tempStart;
+            end = i;
+        }
+    }
+
+    return new int[]{maxSum, start, end}; // 
+}
+
+    public static void main(String args[]) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        // System.out.println(largestSum(arr));
+        // System.out.println(largestSumNeg(arr));
+        System.out.println(Arrays.toString(largestSumRange(arr)));
+
     }
 }
